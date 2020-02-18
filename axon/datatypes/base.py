13dd9ce0cd63a28c41da6864c5d6b3b550211744
mbsantiago <santiago.mbal@gmail.com>
2020-02-17 18:21:41 -0600

    change in API

--- axon/datatypes/base.py
+++ axon/datatypes/base.py
@@ -2,6 +2,105 @@ from abc import ABC
 from abc import abstractmethod
 
 
+def Tuple(array):
+    for dtype in array:
+        if not issubclass(dtype, DataType):
+            message = 'Array value is not a DataType. (type={})'
+            message = message.format(type(value))
+            raise ValueError(message)
+
+    class TupleType(DataType):
+        tuple_data_types = array
+
+        @classmethod
+        def validate(cls, other):
+            if not isinstance(other, (tuple, list)):
+                return False
+
+            if len(other) != len(array):
+                return False
+
+            for other_item, array_dtype in zip(other, array):
+                if not array_dtype.validate(other_item):
+                    return False
+
+            return True
+
+        @classmethod
+        def eq(cls, other):
+            if not issubclass(other, DataType):
+                return False
+
+            if not hasattr(other, 'tuple_data_types'):
+                return False
+
+            if len(cls.tuple_data_types) != len(other.tuple_data_types):
+                return False
+
+            for cls_dtype, other_dtype in zip(cls.tuple_data_types,
+                    other.tuple_data_types):
+                if not cls_dtype.eq(other_dtype):
+                    return False
+
+            return True
+
+    return TupleType
+
+
+def Dict(types):
+    for value in types.values():
+        if not issubclass(value, DataType):
+            message = 'Dictionary value is not a DataType. (type={})'
+            message = message.format(type(value))
+            raise ValueError(message)
+
+    class DictType(DataType):
+        dict_dtypes = types
+
+        @classmethod
+        def validate(cls, other):
+            if not isinstance(other, dict):
+                return False
+
+            for key, value in types.items():
+                if key not in other:
+                    return False
+
+                if not value.validate(other[key]):
+                    return False
+
+            return True
+
+        @classmethod
+        def eq(cls, other):
+            if not issubclass(other, DataType):
+                return False
+
+            if not hasattr(other, 'dict_dtypes'):
+                return False
+
+            for key in cls.dict_dtypes:
+                if not key in other.dict_dtypes:
+                    return False
+
+                cls_dtype = cls.dict_dtypes[key]
+                other_dtype = other.dict_dtypes[key]
+                if not cls_dtype.eq(other_dtype):
+                    return False
+
+            for other_key in other.dict_dtypes:
+                if other_key not in cls.dict_dtypes:
+                    return False
+
+            return True
+
+    return DictType
+
+
+def List(dtype):
+    return dtype.list()
+
+
 class DataType(ABC):
     @classmethod
     @abstractmethod
@@ -11,61 +110,6 @@ class DataType(ABC):
     @classmethod
     def eq(cls, other):
         return other == cls
-
-    @classmethod
-    def new(cls, args):
-        if isinstance(args, dict):
-            return cls.from_dict(args)
-
-        if isinstance(args, (tuple, list)):
-            return cls.from_tuple(args)
-
-        raise ValueError
-
-    @classmethod
-    def from_tuple(cls, array):
-        for dtype in array:
-            if not issubclass(dtype, DataType):
-                message = 'Array value is not a DataType. (type={})'
-                message = message.format(type(value))
-                raise ValueError(message)
-
-        class TupleType(DataType):
-            tuple_data_types = array
-
-            @classmethod
-            def validate(cls, other):
-                if not isinstance(other, (tuple, list)):
-                    return False
-
-                if len(other) != len(array):
-                    return False
-
-                for other_item, array_dtype in zip(other, array):
-                    if not array_dtype.validate(other_item):
-                        return False
-
-                return True
-
-            @classmethod
-            def eq(cls, other):
-                if not issubclass(other, DataType):
-                    return False
-
-                if not hasattr(other, 'tuple_data_types'):
-                    return False
-
-                if len(cls.tuple_data_types) != len(other.tuple_data_types):
-                    return False
-
-                for cls_dtype, other_dtype in zip(cls.tuple_data_types,
-                        other.tuple_data_types):
-                    if not cls_dtype.eq(other_dtype):
-                        return False
-
-                return True
-
-        return TupleType
 
     @classmethod
     def list(cls):
@@ -95,61 +139,17 @@ class DataType(ABC):
 
         return ListType
 
-    @classmethod
-    def from_dict(cls, types):
-        for value in types.values():
-            if not issubclass(value, DataType):
-                message = 'Dictionary value is not a DataType. (type={})'
-                message = message.format(type(value))
-                raise ValueError(message)
-
-        class DictType(DataType):
-            dict_dtypes = types
-
-            @classmethod
-            def validate(cls, other):
-                if not isinstance(other, dict):
-                    return False
-
-                for key, value in types.items():
-                    if key not in other:
-                        return False
-
-                    if not value.validate(other[key]):
-                        return False
-
-                return True
-
-            @classmethod
-            def eq(cls, other):
-                if not issubclass(other, DataType):
-                    return False
-
-                if not hasattr(other, 'dict_dtypes'):
-                    return False
-
-                for key in cls.dict_dtypes:
-                    if not key in other.dict_dtypes:
-                        return False
-
-                    cls_dtype = cls.dict_dtypes[key]
-                    other_dtype = other.dict_dtypes[key]
-                    if not cls_dtype.eq(other_dtype):
-                        return False
-
-                for other_key in other.dict_dtypes:
-                    if other_key not in cls.dict_dtypes:
-                        return False
-
-                return True
-
-        return DictType
-
 
 class String(DataType):
     @classmethod
     def validate(cls, other):
         return isinstance(other, str)
+
+    def __repr__(self):
+        return 'String'
+
+    def __str__(self):
+        return 'String'
 
 
 class Int(DataType):
