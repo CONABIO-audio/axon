c392c5d21f2e19b82968e260992eb1c74b4dd3dd
mbsantiago <santiago.mbal@gmail.com>
2020-02-17 18:33:42 -0600

    added repr and str methods to dtypes

--- axon/datatypes/base.py
+++ axon/datatypes/base.py
@@ -4,7 +4,7 @@ from abc import abstractmethod
 
 def Tuple(array):
     for dtype in array:
-        if not issubclass(dtype, DataType):
+        if not isinstance(dtype, DataType):
             message = 'Array value is not a DataType. (type={})'
             message = message.format(type(value))
             raise ValueError(message)
@@ -12,7 +12,6 @@ def Tuple(array):
     class TupleType(DataType):
         tuple_data_types = array
 
-        @classmethod
         def validate(cls, other):
             if not isinstance(other, (tuple, list)):
                 return False
@@ -26,30 +25,37 @@ def Tuple(array):
 
             return True
 
-        @classmethod
-        def eq(cls, other):
-            if not issubclass(other, DataType):
+        def eq(self, other):
+            if not isinstance(other, DataType):
                 return False
 
             if not hasattr(other, 'tuple_data_types'):
                 return False
 
-            if len(cls.tuple_data_types) != len(other.tuple_data_types):
+            if len(self.tuple_data_types) != len(other.tuple_data_types):
                 return False
 
-            for cls_dtype, other_dtype in zip(cls.tuple_data_types,
+            for self_dtype, other_dtype in zip(self.tuple_data_types,
                     other.tuple_data_types):
-                if not cls_dtype.eq(other_dtype):
+                if not self_dtype.eq(other_dtype):
                     return False
 
             return True
 
-    return TupleType
+        def __repr__(self):
+            reprs = tuple([repr(dtype) for dtype in self.tuple_data_types])
+            return repr(reprs)
+
+        def __str__(self):
+            strs = tuple([str(dtype) for dtype in self.tuple_data_types])
+            return str(strs)
+
+    return TupleType()
 
 
 def Dict(types):
     for value in types.values():
-        if not issubclass(value, DataType):
+        if not isinstance(value, DataType):
             message = 'Dictionary value is not a DataType. (type={})'
             message = message.format(type(value))
             raise ValueError(message)
@@ -57,8 +63,7 @@ def Dict(types):
     class DictType(DataType):
         dict_dtypes = types
 
-        @classmethod
-        def validate(cls, other):
+        def validate(self, other):
             if not isinstance(other, dict):
                 return False
 
@@ -71,30 +76,43 @@ def Dict(types):
 
             return True
 
-        @classmethod
-        def eq(cls, other):
-            if not issubclass(other, DataType):
+        def eq(self, other):
+            if not isinstance(other, DataType):
                 return False
 
             if not hasattr(other, 'dict_dtypes'):
                 return False
 
-            for key in cls.dict_dtypes:
+            for key in self.dict_dtypes:
                 if not key in other.dict_dtypes:
                     return False
 
-                cls_dtype = cls.dict_dtypes[key]
+                self_dtype = self.dict_dtypes[key]
                 other_dtype = other.dict_dtypes[key]
-                if not cls_dtype.eq(other_dtype):
+                if not self_dtype.eq(other_dtype):
                     return False
 
             for other_key in other.dict_dtypes:
-                if other_key not in cls.dict_dtypes:
+                if other_key not in self.dict_dtypes:
                     return False
 
             return True
 
-    return DictType
+        def __repr__(self):
+            dictionary = {
+                key: repr(value)
+                for key, value in self.dict_dtypes.items()
+            }
+            return repr(dictionary)
+
+        def __str__(self):
+            dictionary = {
+                key: str(value)
+                for key, value in self.dict_dtypes.items()
+            }
+            return str(dictionary)
+
+    return DictType()
 
 
 def List(dtype):
@@ -102,73 +120,87 @@ def List(dtype):
 
 
 class DataType(ABC):
-    @classmethod
     @abstractmethod
-    def validate(cls, other):
+    def validate(self, other):
         pass
 
-    @classmethod
-    def eq(cls, other):
-        return other == cls
+    def eq(self, other):
+        return other == self
 
-    @classmethod
-    def list(cls):
+    def list(self):
         class ListType(DataType):
-            list_item_type = cls
+            list_item_type = self
 
-            @classmethod
-            def validate(new_cls, other):
+            def validate(self, other):
                 if not isinstance(other, (tuple, list)):
                     return False
 
                 for item in other:
-                    if not cls.validate(item):
+                    if not self.list_item_type.validate(item):
                         return False
 
                 return True
 
-            @classmethod
-            def eq(cls, other):
-                if not issubclass(other, DataType):
+            def eq(self, other):
+                if not isinstance(other, DataType):
                     return False
 
                 if not hasattr(other, 'list_item_type'):
                     return False
 
-                return cls.list_item_type == other.list_item_type
+                return self.list_item_type == other.list_item_type
 
-        return ListType
+            def __repr__(self):
+                return 'List({})'.format(repr(self.list_item_type))
+
+            def __str__(self):
+                return 'List[{}]'.format(str(self.list_item_type))
+
+        return ListType()
 
 
 class String(DataType):
-    @classmethod
-    def validate(cls, other):
+    def validate(self, other):
         return isinstance(other, str)
 
     def __repr__(self):
-        return 'String'
+        return 'String()'
 
     def __str__(self):
         return 'String'
 
 
 class Int(DataType):
-    @classmethod
-    def validate(cls, other):
+    def validate(self, other):
         return isinstance(other, int)
+
+    def __repr__(self):
+        return 'Int()'
+
+    def __str__(self):
+        return 'Int'
 
 
 class Bool(DataType):
-    @classmethod
-    def validate(cls, other):
+    def validate(self, other):
         return isinstance(other, bool)
+
+    def __repr__(self):
+        return 'Bool()'
+
+    def __str__(self):
+        return 'Bool'
 
 
 class Float(DataType):
-    @classmethod
-    def validate(cls, other):
+    def validate(self, other):
         return isinstance(other, float)
 
+    def __repr__(self):
+        return 'Float()'
+
+    def __str__(self):
+        return 'Float'
 
 # TODO: Implementar Numpy Array
 # TODO: Implementar DataFrame
