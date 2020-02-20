e88197c88017398958ac07b34c27bcccc6803c2d
mbsantiago <santiago.mbal@gmail.com>
2020-02-20 00:45:53 -0600

    added docs to process

--- axon/process.py
+++ axon/process.py
@@ -9,12 +9,73 @@ import logging
 
 
 class Process(ABC):
-    """Process base class."""
+    """Process base class.
+
+    A process is any data transformation function. All process must have a
+    name and should also declare input and output datatypes
+    using axon datatype API.
+
+    All computation done by the process should be defined in the run method.
+
+    Examples
+    --------
+    To create a process that generates spectrograms from a numpy array holding
+    a signal, one could define:
+
+    .. code-block:: python
+        import numpy as np
+        import librosa
+
+        from axon.process import Process
+        import axon.datatypes as dtypes
+
+
+        class SpectrogramMaker(Process):
+            name = 'Spectrogram Maker'
+            input_dtype = dtypes.NumpyArray(np.float, [None])
+            output_dtype = dtypes.NumpyArray(np.float, [513, None])
+
+            def run(self, wav):
+                spec = np.abs(librosa.core.stft(wav, n_fft=1024)
+                return spec
+    """
 
     name = None
+    input_dtype = None
+    output_dtype = None
 
-    def __init__(self):
+    def __init__(self, input_dtype=None, output_dtype=None):
         self.logger = logging.getLogger(self.name)
+
+        if input_dtype is None:
+            input_dtype = self.input_dtype
+        self._input_dtype = input_dtype
+
+        if output_dtype is None:
+            output_dtype = self.output_dtype
+        self._output_dtype = output_dtype
+
+    def get_input_dtype(self):
+        """Get DataType for the input of this process.
+
+        This method usually returns the input data type stated
+        at the class declaration, or the user provided input data type at
+        initialization.
+
+        Rewrite if a more dynamic input data type setting is needed.
+        """
+        return self._input_dtype
+
+    def get_output_dtype(self):
+        """Get DataType for the output of this process.
+
+        This method usually returns the output data type stated
+        at the class declaration, or the user provided output data type at
+        initialization.
+
+        Rewrite if a more dynamic output data type setting is needed.
+        """
+        return self._output_dtype
 
     def info(self, *args, **kwargs):
         """Log message with info level."""
@@ -36,6 +97,6 @@ class Process(ABC):
     def run(self, *inputs, **kwargs):
         """Run the process.
 
-        This method contains all the computation within the process.
+        This method contains all the computation done by the process.
         It must be overwritten by the user.
         """
