57cbf40a8efc7de974e1f2eaf48307f7608db692
dalia1992 <daliaf172@gmail.com>
2020-02-20 19:21:06 -0500

    Add initial tests for pandas dataframe

---
+++ tests/datatypes/test_dataframe_dtypes.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Test module for pandas DataFrame DataTypes."""
+import pandas as pd
+
+import axon.datatypes as dt
+
+def test_validate_dataframe():
+    d = {'col1':[1,2], 'col2':['a', 'c'], 'col3':[0.34,5.5]}
+    df = pd.DataFrame(data=d)
+    D=dt.DataFrame({'col1':dt.Int(), 'col2': dt.String(), 'col3':dt.Float()}, (2,3))
+    assert  D.validate(df)
+    assert not D.validate(d)
+    assert False
