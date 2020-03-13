cc6a7dda6e0e8e55dfb52d574b466889c231ef30
Everardo <erobredo@conabio.gob.mx>
2020-03-13 10:36:31 -0600

    Yesterday changes

--- axon/core/evaluate/evaluator.py
+++ axon/core/evaluate/evaluator.py
@@ -5,8 +5,179 @@ Evaluator module.
 This module defines the base class for all evaluation processes.
 """
 from abc import ABC
-from axon.core.processes import Process
+import os
+import pandas as pd
+from axon.core.processes.mlflow_process import MLFlowProcess
 
 
-class Evaluator(Process, ABC):  # pylint: disable=abstract-method
+class Evaluator(MLFlowProcess, ABC):  # pylint: disable=abstract-method
     """Evaluator class."""
+
+    metrics = None
+    dataset = None
+    model = None
+    output_dir = ""
+    output_name = "evaluation.csv"
+
+    def evaluate_single(self, prediction, target):
+        """Evaluate a single example."""
+        results = []
+        for metric in self.metrics:
+            result = metric(prediction, target)
+            results.append(result)
+
+        return results
+
+    def get_example_iterator(self, dataset):
+        for id, example, target in dataset:
+            yield id, example, target
+
+    def summary(self):
+        """Read or generate evaluation and generate descriptive statistics."""
+
+    def parse_metrics_results(self, results):
+        parsed = {}
+
+        for result in results:
+            parsed.update(result)
+
+        return parsed
+
+    def parse_targets(self, target):
+        return target
+
+    def parse_examples(self, example):
+        return {}
+
+    def parse_predictions(self, prediction):
+        return {}
+
+    def build_model(self):
+        return self.model()
+
+    def build_dataset(self):
+        return self.dataset()
+
+    def get_prediction(self, model, example):
+        return model(example)
+
+    def evaluate(self):
+        model = self.build_model()
+        dataset = self.build_dataset()
+
+        results = []
+        example_iterator = self.get_example_iterator(dataset)
+        for example_id, example, target in example_iterator:
+            prediction = self.get_prediction(model, example)
+            evaluation_result = self.evaluate_single(prediction, target)
+
+            # Get metric results info
+            parsed_results = self.parse_metrics_results(evaluation_result)
+
+            # Get additional metadata
+            parsed_example_metadata = self.parse_examples(example)
+            parsed_prediction_metadata = self.parse_predictions(prediction)
+            parsed_target_metadata = self.parse_targets(target)
+
+            data = {
+                'id': example_id,
+                **parsed_example_metadata,
+                **parsed_target_metadata,
+                **parsed_prediction_metadata,
+                **parsed_results
+            }
+            results.append(data)
+
+        results = pd.DataFrame(results)
+        self.log_results(results)
+        self.save_results(results)
+
+    def get_metric_columns(self):
+        columns = []
+        for metric in self.metrics:
+            columns.append(metric.names)
+        return columns
+
+    def log_results(self, results):
+        metric_columns = self.get_metric_columns()
+        subset = results[metric_columns]
+
+        means = subset.mean().to_dict()
+        self.log_metrics({
+            f'{key}_mean': value
+            for key, value in means.items()
+        })
+
+        stds = subset.std().to_dict()
+        self.log_metrics({
+            f'{key}_std': value
+            for key, value in stds.items()
+        })
+
+    def get_result_path(self):
+        """Get path for result file saving."""
+        relative_path = os.path.join(self.output_dir, self.output_name)
+
+        if self.wdir is not None:
+            return os.path.join(self.wdir, relative_path)
+
+        return relative_path
+
+    def save_results(self, results):
+        """Save dataframe to CSV file."""
+        results.to_csv(path=self.get_result_path())
+
+    def run(self):
+        self.evaluate()
+
+
+class BatchEvaluator(Evaluator):
+    batch_size = 10
+
+    def __init__(self, *args, batch_size=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if batch_size is not None:
+            self.batch_size = batch_size
+
+    def get_example_iterator(self, dataset):
+        for id, example, target in dataset.batch(self.batch_size):
+            yield id, example, target
+
+    def evaluate(self):
+        model = self.build_model(batch_size=self.batch_size)
+        dataset = self.build_dataset()
+
+        results = []
+        example_iterator = self.get_example_iterator(dataset)
+        for example_id_batch, example_batch, target_batch in example_iterator:
+            prediction_batch = self.get_prediction(model, example_batch)
+
+            subiterator = zip(
+                example_id_batch,
+                example_batch,
+                target_batch,
+                prediction_batch)
+            for example_id, example, target, prediction in subiterator:
+                evaluation_result = self.evaluate_single(prediction, target)
+
+                # Get metric results info
+                parsed_results = self.parse_metrics_results(evaluation_result)
+
+                # Get additional metadata
+                parsed_example_metadata = self.parse_examples(example)
+                parsed_prediction_metadata = self.parse_predictions(prediction)
+                parsed_target_metadata = self.parse_targets(target)
+
+                data = {
+                    'id': example_id,
+                    **parsed_example_metadata,
+                    **parsed_target_metadata,
+                    **parsed_prediction_metadata,
+                    **parsed_results
+                }
+                results.append(data)
+
+        results = pd.DataFrame(results)
+        self.log_results(results)
+        self.save_results(results)
--- axon/core/train/trainer.py
+++ axon/core/train/trainer.py
@@ -5,12 +5,39 @@ Trainer module.
 In this module the base class for al Trainer processes is defined.
 """
 from abc import ABC
-from axon.core.processes import Process
+import os
+from axon.core.processes.mlflow_process import MLFlowProcess
 
 
-class Trainer(Process, ABC):  # pylint: disable=abstract-method
+class Trainer(MLFlowProcess, ABC):  # pylint: disable=abstract-method
     """
     Trainer base class.
 
     All training processes must inherit from this class.
     """
+    metrics = None
+    model = None
+    checkpoint_dir = ""
+    batch_size = 1
+    optimizer = None
+    loss = None
+    _step = 0
+
+    def build_model(self):
+        return self.model(batch_size=self.batch_size)
+
+    def load_checkpoint(self, step):
+        """Read checkpoint parameters and load."""
+        ckp_path = os.path.join(self.checkpoint_dir, f'{step}.ckp')
+        if self.wdir is not None:
+            ckp_path = os.path.join(self.wdir, ckp_path)
+        self._step = self.model.load(ckp_path)
+
+    def save_checkpoint(self, step):
+        """Write checkpoint to checkpoint directory."""
+
+    def stop(self, force):
+        """Evaluate stopping criteria and stop if any is true."""
+
+    def train(self, steps=None, epochs=None):
+        """Train model during the specified number of steps or epochs."""
