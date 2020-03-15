a11fafaba9f12421e4aa21dd680f6ac466b840bc
Everardo <erobredo@conabio.gob.mx>
2020-03-15 14:47:49 -0600

    More ideas

--- axon/core/model/base.py
+++ axon/core/model/base.py
@@ -22,7 +22,6 @@ class Model(ABC):
             self._architecture = self.build_architecture()
         return self._architecture
 
-    @abstractmethod
     def preprocess(self, data):
         """Preprocess input data."""
         if self.preprocessor is not None:
@@ -54,17 +53,66 @@ class Model(ABC):
 
     @abstractmethod
     def load(self, path):
-        """Load saved params.
+        """Load persisted model.
 
-        Load model parameters from checkpoint file.
+        Load model with parameters from checkpoint file.
+        """
+
+    @abstractmethod
+    def save(self, path):
+        """Persist model.
+
+        Persist model with current parameters to path.
         """
 
     def __call__(self, data):
         """Run action and return output.
 
-        For consistency and proper record, this method should not be
-        overwritten. If the behaviour of model execution is to be changed,
+        For the sake of consistency and proper record, this method should not
+        be modified. If the behaviour of model execution is to be changed,
         'build_architecture', 'build_preprocessor' or 'build_action' should be
         overwritten instead.
         """
         return self.action(data)
+
+
+import tensorflow as tf
+
+
+class TFModel(Model, ABC):
+    graph = None
+    mode = 'predict'
+    batch_size = 1
+
+    def __init__(self, *args, graph, batch_size=None, **kwargs):
+        if graph is not None:
+            self.graph = graph
+        else:
+            self.graph = tf.Graph()
+        if batch_size is not None:
+            if batch_size <= 0:
+                raise ValueError("Batch size should be greater than 0.")
+            self.batch_size = batch_size
+
+    @abstractmethod
+    def build_preprocessor(self):
+        """Build preprocess pipeline and return callable object.
+
+        This method builds the pre-processing pipeline or imports an object
+        of class Preprocess and returns it.
+        """
+
+    @abstractmethod
+    def build_architecture(self):
+        """Build model architecture.
+
+        This method returns the full model architecture to be instantiated.
+        """
+
+    @abstractmethod
+    def build_action(self):
+        """Build model action.
+
+        This method returns model mapping given by architecture and
+        preprocessing as a callable object that returns model predictions.
+        """
